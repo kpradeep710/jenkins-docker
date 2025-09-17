008890471ad03a9c5c20a4013469c413745ecba4
kpradeep710 <128368784+kpradeep710@users.noreply.github.com>
2025-09-17 11:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-a=10
-b=5
-c=a+b
-print(c)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Flask inside Docker!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
